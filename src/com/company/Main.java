3e4355fa6d8416fc38edb79a731e0992f650685e
Kyle McIntyre <kjmc0707@gmail.com>
2019-10-27 15:27:13 -0400

    seperate network methods

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,41 +1,19 @@
 package com.company;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.*;
-
-
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import com.company.Network;
 
 public class Main {
 
-    private static final String ARP_GET_IP_HW = "arp -a";
-
-    public static String getARPTable(String cmd) throws IOException {
-        Scanner s = new Scanner(Runtime.getRuntime().exec(cmd).getInputStream()).useDelimiter("\\A");
-        return s.hasNext() ? s.next() : "";
-    }
-
     public static void main(String[] args) throws IOException {
-        String table = getARPTable(ARP_GET_IP_HW );
-        String[] arr = table.split(" ");
-        List<String> list = Arrays.asList(arr);
-        List<String> nums = new ArrayList<>();
-        int c = 0;
-        while(c < list.size()){
-            if(list.get(c).matches(".*\\d.*")){
-                nums.add(list.get(c));
-            }
-            c++;
+        List<String> ipAndMac = Network.getIPAndMac();
+        for(String address : ipAndMac){
+            System.out.println(address);
         }
-        for(int i = 0; i<nums.size(); i++){
-            if(nums.get(i).contains("192.168")){
-                if(nums.get(i+1).contains("-")){
-                    System.out.println("IP: " + nums.get(i) + "  MAC: " + nums.get(i+1));
-                }
-            }
-        }
-        //System.out.println(getARPTable(ARP_GET_IP_HW ));
     }
     
     // private static final Integer PING_TIMEOUT = 5000;
